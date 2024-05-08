31348fd2ad6cfb9b4812161c5892009de90e2a17
mithileshpatel888 <mithleshpatel887766@gmail.com>
2024-05-08 16:19:24 +0530

    adding the all file camelCase

--- src/main/java/com/ani/backend/controllers/pdfController.java
+++ src/main/java/com/ani/backend/controllers/pdfController.java
@@ -4,34 +4,30 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import com.ani.backend.service.pdfService;
-
-
-import jakarta.servlet.http.HttpServletResponse;
+ import org.springframework.web.bind.annotation.GetMapping;
+ import com.ani.backend.service.PdfService;
+ import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class PdfController {
 
-    private final pdfService Pdfservice;
+    private final PdfService pdfService;
 
-    public PdfController(pdfService Pdfservice) {
-
-        this.Pdfservice = Pdfservice;
+    PdfController(PdfService pdfService) {
+        this.pdfService = pdfService;
     }
 
-    @GetMapping("/pdf/generate")
+
+    @GetMapping("pdf/generate")
     public void generatePdf(HttpServletResponse response) throws IOException {
         response.setContentType("application/pdf");
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd:hh:mm:ss");
-        
-        String currentDateTime = dateFormat.format(new Date());
-        String headerkey = "Content-Disposition";
-        String headervalue = "attachment; filename=pdf_" + currentDateTime + ".pdf";
-        response.setHeader(headerkey, headervalue);
-
-        this.Pdfservice.export(response);
+         DateFormat dateFormat =  new SimpleDateFormat("yyyy-mm-dd:hh:mm:ss");
+         String currentDateTime = dateFormat.format(new Date());
+         String headerKey = "Content-Disposition";
+         String headerValue = "attachment; filename=pdf_" + currentDateTime + ".pdf";
+         response.setHeader(headerKey, headerValue);
+         this.pdfService.export(response);
     }
 }
