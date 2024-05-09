e5147b1a137f5b2fe393a68907368b8f0f9c5ed8
mithileshpatel888 <mithleshpatel887766@gmail.com>
2024-05-09 15:29:59 +0530

    remove final keyword

--- src/main/java/com/ani/backend/controllers/pdfController.java
+++ src/main/java/com/ani/backend/controllers/pdfController.java
@@ -1,25 +1,24 @@
 package com.ani.backend.controllers;
-
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
- import org.springframework.web.bind.annotation.GetMapping;
- import com.ani.backend.service.PdfService;
- import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.ani.backend.service.PdfService;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class PdfController {
+    @Autowired
+    private PdfService pdfService;
 
-    private final PdfService pdfService;
-
-    PdfController(PdfService pdfService) {
+   
+    public PdfController(PdfService pdfService) {
         this.pdfService = pdfService;
     }
-
-
+   
     @GetMapping("pdf/generate")
     public void generatePdf(HttpServletResponse response) throws IOException {
         response.setContentType("application/pdf");
